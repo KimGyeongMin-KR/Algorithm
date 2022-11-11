15390a65e9db2b0ae89f35e24712f2122a2ca84f
Kim GyeongMin <gyeongmin951@naver.com>
2022-11-11 17:17:14 +0900

    Add : 소수 만들기 정답 코드

---
+++ rabbit-week-2/creating_decimal_number.py
@@ -0,0 +1,17 @@
+from itertools import combinations
+
+def solution(nums):
+
+    sum_list = list(map(lambda x: sum(x), list(combinations(nums, 3))))
+    cnt = 0
+    
+    for i in list(sum_list):
+        for d in range(2,int(i**0.5) + 1):
+            if i % d == 0:
+                break
+        else:
+            cnt += 1
+
+    return cnt if cnt!=0 else 0
+
+# combinations 쓰지 않고 만들어 보기
