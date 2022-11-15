a87ebc61ff6c2b80debde5e933db05609a0909c3
Kim GyeongMin <gyeongmin951@naver.com>
2022-11-15 19:38:20 +0900

    Update : 실패율 개선 코드

--- rabbit-week-2/failure_rate.py
+++ rabbit-week-2/failure_rate.py
@@ -14,3 +14,24 @@ def solution(N, stages):
     answer = [ x[1] for x in failer_sorted ]
     
     return answer
+
+# 개선 코드
+# sum()함수 있을 때 O(N^2)
+# sorted() O(NlogN)
+def solution(N, stages):
+
+    stage = [0] * (N+2)
+    fail_per = [0] * (N+2)
+    reached = len(stages)
+
+    for st in stages:
+        stage[st] += 1
+    
+    for st in range(1,len(stage)):
+        reached -= stage[st-1]
+        fail_per[st] = (stage[st]/reached, st) if stage[st] else (0,st)
+        
+    failer_sorted = sorted(fail_per[1:-1], key = lambda x : (-x[0], x[1]))
+    answer = [ x[1] for x in failer_sorted ]
+    
+    return answer
