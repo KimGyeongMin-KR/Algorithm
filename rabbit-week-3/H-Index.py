dcd8a6b83cc5e9c2a0ab7907073a861081069c51
Kim GyeongMin <gyeongmin951@naver.com>
2022-11-18 01:15:24 +0900

    Fix & Update : 반례 확인 & 보완 코드 & 간단한 답 추가

--- rabbit-week-3/H-Index.py
+++ rabbit-week-3/H-Index.py
@@ -1,15 +1,56 @@
+### 정답 코드 반례 존재 ###
 def solution(m):
     answer = 0
     m_idx = 0
     m.sort()
-    for i in range(max(m)+1):
+    for h in range(max(m)+1):
         
-        if m[m_idx] < i:
+        if m[m_idx] < h:
             m_idx += 1
             
-        if len(m[m_idx:]) >= i:
-            answer = i
+        if len(m[m_idx:]) >= h:
+            answer = h
         else:
             break
             
     return answer
+
+# 반례
+# 입력 : [0,1,1,1,1,1,2,3,4]
+# 정답 : 2
+# 출력 : 4
+
+
+# 보완한 답 0.002s
+def solution(m):
+    answer = 0
+    m_idx = 0
+    m.sort()
+    for h in range(max(m)+1):
+        
+        if m[m_idx] < h:
+            while m[m_idx] < h:
+                m_idx += 1
+                if m_idx == len(m)-1:
+                    break
+            
+        if len(m[m_idx:]) >= h:
+            answer = h
+        else:
+            break
+            
+    return answer
+
+
+# 간단한 답 : 0.007s
+def solution(m):
+    
+    h = 0
+    
+    for h in range(len(m), -1,-1):
+        count = 0
+        for num in m:
+            if num >= h:
+                count += 1
+        if count >= h:
+            return h
