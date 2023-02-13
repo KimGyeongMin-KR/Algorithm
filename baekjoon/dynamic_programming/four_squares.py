94dd43021df127e6d575ea8cd7a853d3556aacfe
Kim GyeongMin <gyeongmin951@naver.com>
2023-02-13 23:07:15 +0900

    [ Add ] S3 four_squares DP

---
+++ baekjoon/dynamic_programming/four_squares.py
@@ -0,0 +1,9 @@
+n = int(input())
+dp = [0] * (n + 1)
+dp[1] = 1
+for i in range(2, n + 1):
+    minimum = float('inf')
+    for j in range(1, int(i**0.5) + 1):
+        minimum = min(minimum, dp[i - j**2])
+    dp[i] = minimum + 1
+print(dp[n])
