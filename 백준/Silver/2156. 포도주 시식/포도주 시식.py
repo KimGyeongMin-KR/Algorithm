092247c7d79598e7bcd36f1501cd3399c72356e0
김경민 <78214692+KimGyeongMin-KR@users.noreply.github.com>
2024-03-17 15:58:55 +0900

    [Silver I] Title: 포도주 시식, Time: 428 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,13 @@
+N = int(input())
+input_list = [0] * 3 + [int(input()) for _ in range(N)]
+dp = [0] * 3 + [0] * N
+
+def solution():
+    for i in range(3, N + 3):
+        dp[i] = max(
+            input_list[i] + input_list[i - 1] + dp[i - 3],
+            input_list[i] + dp[i - 2],
+            dp[i - 1],
+        )
+    return max(dp[3:])
+print(solution())
