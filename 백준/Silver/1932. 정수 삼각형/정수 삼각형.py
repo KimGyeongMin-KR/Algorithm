e38c774da48b4082ee6d768593ec23774ce9aa84
김경민 <78214692+KimGyeongMin-KR@users.noreply.github.com>
2024-03-25 17:17:40 +0900

    [Silver I] Title: 정수 삼각형, Time: 132 ms, Memory: 35616 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,14 +1,25 @@
+# N = int(input())
+# p = [[0]] + [list(map(int, input().split())) for _ in range(N)]
+# for i in range(1, N + 1):
+#     for j in range(i):
+#         if j == 0:
+#             p[i][j] = p[i-1][0] + p[i][j]
+#         elif j == i - 1:
+#             p[i][j] = p[i-1][-1] + p[i][j]
+#         else:
+#             p[i][j] = max(
+#                 p[i-1][j-1],
+#                 p[i-1][j],
+#             ) + p[i][j]
+
+import sys
+input = sys.stdin.readline
 N = int(input())
-p = [[0]] + [list(map(int, input().split())) for _ in range(N)]
-for i in range(1, N + 1):
-    for j in range(i):
-        if j == 0:
-            p[i][j] = p[i-1][0] + p[i][j]
-        elif j == i - 1:
-            p[i][j] = p[i-1][-1] + p[i][j]
-        else:
-            p[i][j] = max(
-                p[i-1][j-1],
-                p[i-1][j],
-            ) + p[i][j]
-print(max(p[-1]))
+p = [list(map(int, input().split())) for _ in range(N)]
+for i in range(N-2, -1, -1):
+    for j in range(i+1):
+        p[i][j] = max(
+            p[i+1][j],
+            p[i+1][j+1],
+        ) + p[i][j]
+print(p[0][0])
