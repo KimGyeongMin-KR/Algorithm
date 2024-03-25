4ad726b684cbe353b0c0172ed196c41552b1b1a9
김경민 <78214692+KimGyeongMin-KR@users.noreply.github.com>
2024-03-25 15:51:19 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,15 +1,11 @@
 N = int(input())
-dp = [0 for _ in range(N)]
-time_table = [list(map(int, input().split())) for _ in range(N)]
+tt = [list(map(int, input().split())) for _ in range(N)]
+dp = [0] * (N + 1)
 
-for i in range(N):
-    d, m = time_table[i]
-    if d + i >= N + 1:
-        continue
-    dp[i] = max(dp[i], m)
-    for j in range(i):
-        due, _ = time_table[j]
-        if  due + j >= i + 1:
-            continue
-        dp[i] = max(dp[i], dp[j] + m)
+for idx in range(N-1, -1, -1):
+    due, money = tt[idx]
+    if due + idx > N:
+        dp[idx] = dp[idx + 1]
+    else:
+        dp[idx] = max(dp[idx + 1], money + dp[due + idx])
 print(max(dp))
